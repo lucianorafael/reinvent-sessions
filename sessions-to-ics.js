685d945bda6e2aa9fc93ac114ce0c05e32639a15
Gordon Shankman <gordon.shankman@bti360.com>
2023-10-17 13:30:14 -0400

    Update to write registered events

--- sessions-to-ics.js
+++ sessions-to-ics.js
@@ -1,6 +1,19 @@
 const fs = require("fs");
 const ics = require("ics");
-const { program } = require('commander');
+const { DateTime } = require("luxon");
+const { program } = require("commander");
+
+const CSV_HEADINGS = {
+    startDay: "Start Day",
+    startTime: "Start Time",
+    endDay: "End Day",
+    endTime: "End Time",
+    venue: "Venue",
+    room: "Room",
+    sessionType: "Session Type",
+    sessionId: "Session ID",
+    title: "Title",
+};
 
 function normalizeFilename(filename) {
     return filename.toLowerCase().replace(/[^a-z]+/, '-');
@@ -18,6 +31,16 @@ function writeEvents(eventList, outputDir, filename) {
     });
 }
 
+function writeCsv(eventList, outputDir, filename) {
+    filename = normalizeFilename(filename);
+    const eventsWithHeadings = [CSV_HEADINGS, ...eventList];
+    const outputEvents = eventsWithHeadings.map((e) => {
+        return `${e.startDay},${e.startTime},${e.endDay},${e.endTime},"${e.venue}","${e.room}",${e.sessionType},"${e.sessionId}","${e.title}"`;
+    });
+    fs.writeFileSync(`${outputDir}/${filename}.csv`, outputEvents.join("\n"));
+    console.log(`Wrote ${eventList.length} events to ${outputDir}/${filename}.csv`);
+}
+
 function loadSessionsByID(sessionsFile) {
     const sessions = JSON.parse(fs.readFileSync(sessionsFile));
     return sessions.data.reduce((a, v) => ({ ...a, [v.scheduleUid]: v}), {});
@@ -26,6 +49,11 @@ function loadSessionsByID(sessionsFile) {
 function loadInterests(interestsFile, sessions) {
     const interests = JSON.parse(fs.readFileSync(interestsFile));
     return interests.data.followedSessions.map((interest) => sessions[interest.scheduleUid]);
+}
+
+function loadReserved(interestsFile, sessions) {
+    const interests = JSON.parse(fs.readFileSync(interestsFile));
+    return interests.data.userReservationSessions.map((reservation) => sessions[reservation.scheduleUid]);
 }
 
 function toIcsDateTime(unixTimeSec) {
@@ -38,6 +66,12 @@ function toIcsDateTime(unixTimeSec) {
         date.getUTCHours(),
         date.getUTCMinutes()
     ];
+}
+
+function toCsvDateTime(unixTimeSec) {
+    const unixTimeMs = unixTimeSec * 1000;
+    const date = DateTime.fromMillis(unixTimeMs).setZone("America/Los_Angeles");
+    return { day: date.toFormat("EEE"), time: date.toFormat("HH:mm") }
 }
 
 function sessionToIcs(session) {
@@ -53,27 +87,51 @@ function sessionToIcs(session) {
     return { sessionType, event };
 }
 
+function sessionToCsv(session) {
+    const start = toCsvDateTime(session.startDateTime);
+    const end = toCsvDateTime(session.endDateTime);
+    return {
+        startDay: start.day,
+        startTime: start.time,
+        endDay: end.day,
+        endTime: end.time,
+        venue: session.venueName,
+        room: session.locationName,
+        sessionType: session.sessionType,
+        sessionId: session.thirdPartyID.replace(/\s+/g, ""),
+        title: session.title,
+    };
+}
+
 function interestsToICS(sessionsFile, interestsFile, options, command) {
     const sessions = loadSessionsByID(sessionsFile);
     const interests = loadInterests(interestsFile, sessions);
+    const reservations = loadReserved(interestsFile, sessions);
     const outputDir = options.outputDir;
 
     fs.mkdirSync(`./${outputDir}`, {recursive: true});
 
-    const events = {};
-    interests.map(sessionToIcs).forEach((session) => {
-        if (!events.hasOwnProperty(session.sessionType)) {
-            events[session.sessionType] = [];
-        }
-        events[session.sessionType].push(session.event);
-    });
+    if (!options.reservedOnly) {
+        const events = {};
+        interests.map(sessionToIcs).forEach((session) => {
+            if (!events.hasOwnProperty(session.sessionType)) {
+                events[session.sessionType] = [];
+            }
+            events[session.sessionType].push(session.event);
+        });
 
-    let allEvents = [];
-    for (const eventType in events) {
-        allEvents = allEvents.concat(events[eventType]);
-        writeEvents(events[eventType], outputDir, `${eventType}s`);
+        let allEvents = [];
+        for (const eventType in events) {
+            allEvents = allEvents.concat(events[eventType]);
+            writeEvents(events[eventType], outputDir, `${eventType}s`);
+        }
+        writeEvents(allEvents, outputDir, 'all-sessions');
+
+        const csvEvents = interests.map(sessionToCsv);
+        writeCsv(csvEvents, outputDir, 'all-sessions');
     }
-    writeEvents(allEvents, outputDir, 'all-sessions');
+
+    writeEvents(reservations.map((r) => sessionToIcs(r).event), outputDir, "reserved");
 }
 
 program
@@ -81,6 +139,7 @@ program
     .version('2023.0.0')
     .showHelpAfterError(true)
     .option('-o, --output-dir <dir>', 'the output directory', 'sessions')
+    .option('-r, --reserved-only', 'Only output reserved sessions')
     .argument('<sessions>', 'the session catalog')
     .argument('<interests>', 'the interests file')
     .action(interestsToICS)
